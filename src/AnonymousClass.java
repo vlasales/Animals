e25bed2f5529173aec27ed79987ffddace2bc874
Vlastimil Pospichal <38721480+vlasales@users.noreply.github.com>
2020-05-25 19:01:00 +0200

    Update AnonymousClass.java

--- src/AnonymousClass.java
+++ src/AnonymousClass.java
@@ -2,6 +2,11 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 class AnonymousClass extends TimerTask {
+
+    public void AnonymousClass (long start) {
+        this.start = start;
+    }
+
     @Override
     public void run () {
         System.out.print("Task invoked: " +
@@ -13,7 +18,7 @@ class AnonymousClass extends TimerTask {
     public static void main() {
         Timer timer = new Timer("Timer");
         long start = System.currentTimeMillis();
-        TimerTask task = new AnonymousClass();
+        TimerTask task = new AnonymousClass(start);
 
         long delay = 1000L;
         timer.schedule(task, delay);
