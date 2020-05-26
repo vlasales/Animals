5cb86f2e0a447886e6588a2fe930d7ed244819f1
Vlastimil Pospichal <38721480+vlasales@users.noreply.github.com>
2020-05-26 16:03:05 +0200

    Update Histogram.java

--- src/Histogram.java
+++ src/Histogram.java
@@ -4,30 +4,26 @@ public class Histogram{
 
     public static void main(String []args){
         Map<String, Integer> records = new HashMap<String, Integer>();
-    //    try {
-    //BufferedReader reader = new BufferedReader(new FileReader(filename));
-   //while ((String line = reader.readLine()) != null) {
-        StringTokenizer tokenizer = new StringTokenizer( 
-                "Lets . practice, at. at GeeksforGeeks",
-                ",. "
-                ); 
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            while ((String line = reader.readLine()) != null) {
+                StringTokenizer tokenizer = new StringTokenizer( 
+                   "Lets . practice, at. at GeeksforGeeks",
+                    ",. "
+                    ); 
 
-        // Displaying the Tokens 
-        while (tokenizer.hasMoreTokens()) { 
-            String word = tokenizer.nextToken();
-            if (records.get(word) == null){
-                records.put(word, 1);
-            } else {
-                records.put(word, records.get(word) + 1);
-            }
-        }
-        System.out.println(records);
-        for(Map.Entry<String, Integer> element : records.entrySet()) {
-            String key = element.getKey();
-            Integer value = element.getValue();
-            System.out.println(key + ": " + value);
-        }
-        boolean DESC = false;
+                // Displaying the Tokens 
+                while (tokenizer.hasMoreTokens()) { 
+                    String word = tokenizer.nextToken();
+                    records.put(word, (records.get(word) == null) ? 1 : records.get(word) + 1);
+                }
+                System.out.println(records);
+                for(Map.Entry<String, Integer> element : records.entrySet()) {
+                    String key = element.getKey();
+                    Integer value = element.getValue();
+                    System.out.println(key + ": " + value);
+                }
+                boolean DESC = false;
     //    Map<String, Integer> sorted = sortByComparator(records, DESC);
      //   System.out.println(sorted);
     //}
